6f52712d7de4570392959c0af3242de4fdbb7961
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-06-29 19:54:09 -0500

    main page test

--- server.js
+++ server.js
@@ -13,3 +13,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 console.log("Hi");
+
+
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+});
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
