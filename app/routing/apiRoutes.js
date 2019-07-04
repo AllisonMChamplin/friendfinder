36f94912b28bacdac6d1457fd6de056bb14fb8ef
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-07-03 23:53:19 -0500

    Updated test data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,6 @@ module.exports = function (app) {
     app.post('/api/survey', function (req, res) {
         console.log("hi");
         userData.push(req.body);
-        // res.json(userData);
     });
 
     app.post('/api/clear', function (req, res) {
