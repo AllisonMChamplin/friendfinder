2d452a7d83b52fdf0dcc2ab9600e77393a19dfd3
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-07-05 23:43:59 -0500

    Matching logic

--- app/public/match.js
+++ app/public/match.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    var userListDiv = $('#user-list');
+
     function runUserQuery() {
         var currentURL = window.location.origin;
         $.ajax({
@@ -7,26 +9,76 @@ $(document).ready(function () {
             method: "GET"
         })
             .done(function (userData) {
-                ajaxTest(userData);
+                testMany(userData);
             });
     };
 
-    function ajaxTest(userData) {
+    function testMany(userData) {
+        console.log("testMany function:");
+        // userListDiv.append("Here's the userData looped names at first: ");
+        // for (var i = 0; i < userData.length; i++) {
+        //     userListDiv.append(JSON.stringify(userData[i].userName));
+        // };
+        // var userOne = userData.splice(0, 1);
+        // userListDiv.append("<br><br>Here's the userOne after splice: ");
+        //     userListDiv.append(JSON.stringify(userOne));
+        // userListDiv.append("<br><br>Here's the userData looped names after splice: ");
+        // for (var i = 0; i < userData.length; i++) {
+        //     userListDiv.append(JSON.stringify(userData[i].userName));
+        // };
+
+
+        var userOne = userData.splice(0, 1);
+        var userArray = userData;
+
+        for (var i = 0; i < userArray.length; i++) {
+
+            userListDiv.append(JSON.stringify(userOne[0].userName) + " VS " + JSON.stringify(userArray[i].userName));
+            userListDiv.append("<br>");
+
+            var tempComparisonArray = [];
+
+            for (var j = 0; j < userArray.length; j++) {
+                var x = userOne[0].scores[j];
+                var y = userArray[i].scores[j];
+                var z = x - y;
+                tempComparisonArray.push(Math.abs(z));
+                // userListDiv.append(tempComparisonArray);
+            }
+            userListDiv.append(tempComparisonArray);
+
+            let sum = tempComparisonArray.reduce((acc, val) => {
+                return acc + val;
+            });
+            console.log(sum);
+            userListDiv.append("<h2>Score: " + sum + "</h2>");
+
+            userListDiv.append("<br><br>");
+
+            // console.log("userOne.scores", userOne[0].scores);
+            // console.log("userArray[i].scores", userArray[i].scores);
+
+        }
+
+
+    };
+
+    function ajaxTest(userData, userOne) {
         console.log("ajaxTest function:");
 
-        var userOne = userData[0].scores;
-        var userTwo = userData[1].scores;
+        var tempTwo = userData.slice(1);
+        var userTwo = userData[0].scores;
         var differenceArray = [];
         var compatabilityScore = 0;
 
         for (var i = 0; i < userOne.length; i++) {
-            console.log("userOne - userTwo", userOne[i] + "-" + userTwo[i]);
+            // console.log("userOne - userTwo", userOne[i] + "-" + userTwo[i]);
             var x = Math.abs(userOne[i] - userTwo[i]);
-            console.log("x: ", x);
+            // console.log("x: ", x);
             differenceArray.push(x);
             compatabilityScore += x;
-            console.log("differenceArray: ", differenceArray);
-            console.log("compat: ", compatabilityScore);
+            // console.log("differenceArray: ", differenceArray);
+            // console.log("compat: ", compatabilityScore);
         }
 
         var wrap = $("<div class='wrap' class='clearfix'>");
@@ -44,6 +96,7 @@ $(document).ready(function () {
             wrap.append(a);
         };
         targetDiv.append(wrap);
+
     };
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
 
     app.get('/api/users', function (req, res) {
         res.json(userData);
-    })
+    });
 
     app.post('/api/survey', function (req, res) {
         console.log("hi");
